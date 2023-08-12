994f3153e237979a730a7b3ea7b3a65e648e7ec7
Brent Westbrook <brentrwestbrook@gmail.com>
2023-08-12 14:16:11 -0400

    make graph even more generic with edge types, add descriptor api

--- src/graph.rs
+++ src/graph.rs
@@ -1,37 +1,61 @@
 use std::ops::{Index, IndexMut};
 
+#[derive(Clone, Copy)]
+pub(crate) struct VertexDescriptor(pub(crate) usize);
+
+#[derive(Clone, Copy)]
+pub(crate) struct EdgeDescriptor(pub(crate) usize);
+
 #[derive(Default)]
-pub(crate) struct Graph<T> {
-    vertices: Vec<T>,
+pub(crate) struct Graph<V, E> {
+    pub(crate) vertices: Vec<V>,
+    pub(crate) edges: Vec<E>,
 }
 
-impl<T> Graph<T>
+impl<V, E> Graph<V, E>
 where
-    T: Default,
+    V: Default,
+    E: Default,
 {
     /// allocate a new vertex and return its index
-    pub(crate) fn add_vertex(&mut self) -> usize {
+    pub(crate) fn add_vertex(&mut self) -> VertexDescriptor {
         let next = self.vertices.len();
-        self.vertices.push(T::default());
-        next
+        self.vertices.push(V::default());
+        VertexDescriptor(next)
     }
 
-    /// insert a new edge between
-    pub(crate) fn add_edge(&self, atom1: usize, atom2: usize) -> usize {
-        todo!()
-    }
-}
-
-impl<T> Index<usize> for Graph<T> {
-    type Output = T;
-
-    fn index(&self, index: usize) -> &Self::Output {
-        &self.vertices[index]
+    /// allocate a new edge and return its index
+    pub(crate) fn add_edge(&mut self) -> EdgeDescriptor {
+        let next = self.edges.len();
+        self.edges.push(E::default());
+        EdgeDescriptor(next)
     }
 }
 
-impl<T> IndexMut<usize> for Graph<T> {
-    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
-        &mut self.vertices[index]
+impl<V, E> Index<VertexDescriptor> for Graph<V, E> {
+    type Output = V;
+
+    fn index(&self, index: VertexDescriptor) -> &Self::Output {
+        &self.vertices[index.0]
+    }
+}
+
+impl<V, E> IndexMut<VertexDescriptor> for Graph<V, E> {
+    fn index_mut(&mut self, index: VertexDescriptor) -> &mut Self::Output {
+        &mut self.vertices[index.0]
+    }
+}
+
+impl<V, E> Index<EdgeDescriptor> for Graph<V, E> {
+    type Output = E;
+
+    fn index(&self, index: EdgeDescriptor) -> &Self::Output {
+        &self.edges[index.0]
+    }
+}
+
+impl<V, E> IndexMut<EdgeDescriptor> for Graph<V, E> {
+    fn index_mut(&mut self, index: EdgeDescriptor) -> &mut Self::Output {
+        &mut self.edges[index.0]
     }
 }
