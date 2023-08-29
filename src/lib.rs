e15dd0aea1914fd9b56c639faad7cb22b0468ccd
Brent Westbrook <brentrwestbrook@gmail.com>
2023-08-29 11:03:30 -0400

    nightmarish ParseMolFileAtomLine with most unimplemented

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+#![allow(unused)]
 #![feature(lazy_cell)]
 
 use std::{
@@ -16,6 +17,8 @@ use crate::ptable::{OUTER_ELECS, SYMBOL};
 pub mod bond;
 mod graph;
 mod ptable;
+
+const COMPLEX_QUERIES: [&str; 8] = ["A", "AH", "Q", "QH", "X", "XH", "M", "MH"];
 
 #[derive(Default)]
 pub enum Chi {
@@ -109,7 +112,7 @@ pub enum AromaticityModel {
     Default,
 }
 
-#[derive(Clone, Default)]
+#[derive(Clone, Debug, Default)]
 #[allow(unused)]
 struct Point3D {
     x: f64,
@@ -122,7 +125,8 @@ impl Point3D {
         Self { x, y, z }
     }
 
-    fn zero() -> Self {
+    #[inline]
+    const fn zero() -> Self {
         Self {
             x: 0.0,
             y: 0.0,
@@ -194,6 +198,7 @@ impl RWMol {
     /// load an [RWMol] from an SDF file
     pub fn from_sdf(file: impl AsRef<Path>) -> Self {
         let s = read_to_string(dbg!(file.as_ref())).unwrap();
+        println!("{s}");
         let mut lines = s.lines();
 
         let name = lines.next().unwrap();
@@ -235,6 +240,193 @@ impl RWMol {
                     let line = lines.next().unwrap();
                     // in-line ParseMolFileAtomLine
                     let (atom, pos) = {
+                        if line.len() < 34 {
+                            // we're always strict, or we could check < 32
+                            // without
+                            panic!("atom line too short");
+                        }
+                        let mut pos = Point3D::zero();
+                        pos.x = line[0..10].trim().parse().unwrap();
+                        pos.y = line[10..20].trim().parse().unwrap();
+                        pos.z = line[20..30].trim().parse().unwrap();
+                        let symb = line[31..34].trim();
+                        // REVIEW: should we handle missing fields at the end of
+                        // the line?
+                        let mut mass_diff = 0;
+                        if line.len() >= 36 && &line[34..36] != " 0" {
+                            mass_diff = line[34..36].trim().parse().unwrap();
+                        }
+                        let mut chg = 0;
+                        if line.len() >= 39 && &line[36..39] != "  0" {
+                            chg = line[36..39].trim().parse().unwrap();
+                        }
+                        let mut hcount = 0;
+                        if line.len() >= 45 && &line[42..45] != "  0" {
+                            hcount = line[42..45].trim().parse().unwrap();
+                        }
+                        let mut res = Atom::default();
+                        let is_complex_query_name = COMPLEX_QUERIES
+                            .iter()
+                            .find(|&&s| s == symb)
+                            .is_some();
+                        if is_complex_query_name
+                            || symb == "L"
+                            || symb == "LP"
+                            || symb == "R"
+                            || symb == "R#"
+                            || (symb.chars().nth(0).unwrap() == 'R'
+                                && symb >= "R0"
+                                && symb <= "R99")
+                        {
+                            if is_complex_query_name
+                                || symb == "*"
+                                || symb == "R"
+                            {
+                                let mut query = Atom::new(0);
+                                if symb == "*" || symb == "R" {
+                                    // according to the MDL spec, these match
+                                    // anything
+                                    query.set_query(make_atom_null_query());
+                                } else if is_complex_query_name {
+                                    query.convert_complex_name(symb);
+                                }
+                                res = query;
+                                res.set_no_implicit(true);
+                            } else {
+                                res.set_atomic_num(0);
+                            }
+                            if mass_diff == 0
+                                && symb.chars().nth(0).unwrap() == 'R'
+                            {
+                                if symb.len() > 1
+                                    && symb >= "R0"
+                                    && symb <= "R99"
+                                {
+                                    let rnumber = &symb[1..symb.len() - 1]
+                                        .parse()
+                                        .unwrap_or(-1);
+                                    if *rnumber >= 0 {
+                                        res.set_isotope(*rnumber);
+                                    }
+                                }
+                            }
+                            if symb.chars().nth(0).unwrap() == 'R' {
+                                todo!("setRGPProps(symb, res)");
+                            }
+                        } else if symb == "D" {
+                            res.set_atomic_num(1);
+                            res.set_isotope(2);
+                        } else if symb == "T" {
+                            res.set_atomic_num(1);
+                            res.set_isotope(3);
+                        } else if symb == "Pol" || symb == "Mod" {
+                            res.set_atomic_num(0);
+                            res.dummy_label = symb.to_owned();
+                        } else {
+                            let s: Vec<_> = symb.chars().collect();
+                            let symb = if symb.len() == 2
+                                && s[1] >= 'A'
+                                && s[1] <= 'Z'
+                            {
+                                format!("{}{}", s[0], s[1].to_ascii_lowercase())
+                            } else {
+                                symb.to_string()
+                            };
+                            lookup_atomic_number(res, symb);
+                        }
+                        if chg != 0 {
+                            res.set_formal_charge(chg);
+                        }
+                        if (hcount >= 1) {
+                            // NOTE: skipping res->hasQuery() check here, 1531
+                            // MolFileParser.cpp
+                            res.set_no_implicit(true);
+                            if (hcount > 1) {
+                                todo!()
+                            //                       ATOM_EQUALS_QUERY *oq = makeAtomImplicitHCountQuery(hcount - 1);
+                            //                       auto nq = makeAtomSimpleQuery<ATOM_LESSEQUAL_QUERY>(
+                            // hcount - 1, oq->getDataFunc(),
+                            // std::string("less_") + oq->getDescription());
+                            //                       res->expandQuery(nq);
+                            //                       delete oq;
+                            } else {
+                                // res->expandQuery(makeAtomImplicitHCountQuery(0));
+                                todo!();
+                            }
+                        }
+
+                        if (mass_diff != 0) {
+                            todo!()
+                            // int defIso =
+                            //     PeriodicTable::getTable()->getMostCommonIsotope(res->getAtomicNum());
+                            // int dIso = defIso + mass_diff;
+                            // if (dIso < 0) {
+                            //   BOOST_LOG(rdWarningLog)
+                            //       << " atom " << res->getIdx()
+                            //       << " has a negative isotope offset. line:  " << line << std::endl;
+                            // }
+                            // res->setIsotope(dIso);
+                            // res->setProp(common_properties::_hasMassQuery, true);
+                        }
+
+                        if (line.len() >= 42 && &line[39..42] != "  0") {
+                            let mut parity = 0;
+                            parity = line[39..42].trim().parse().unwrap();
+                            res.set_parity(parity);
+                        }
+
+                        if (line.len() >= 48 && &line[45..45 + 3] != "  0") {
+                            let mut stereoCare = 0;
+                            stereoCare =
+                                line[45..45 + 3].trim().parse().unwrap();
+                            res.set_stereo_care(stereo_care);
+                        }
+                        if (line.len() >= 51 && &line[48..48 + 3] != "  0") {
+                            let mut totValence = 0;
+                            totValence =
+                                line[48..48 + 3].trim().parse().unwrap();
+                            if (totValence != 0) {
+                                // only set if it's a non-default value
+                                res.set_tot_valence(totValence);
+                            }
+                        }
+                        if (line.len() >= 57 && &line[54..54 + 3] != "  0") {
+                            let mut rxnRole = 0;
+                            rxnRole = line[54..54 + 3].trim().parse().unwrap();
+                            if (rxnRole != 0) {
+                                // only set if it's a non-default value
+                                res.set_molRxnRole(rxnRole);
+                            }
+                        }
+                        if (line.len() >= 60 && &line[57..57 + 3] != "  0") {
+                            let mut rxnComponent = 0;
+                            rxnComponent =
+                                line[57..57 + 3].trim().parse().unwrap();
+                            if (rxnComponent != 0) {
+                                // only set if it's a non-default value
+                                res.set_molRxnComponent(rxnComponent);
+                            }
+                        }
+                        if (line.len() >= 63 && &line[60..60 + 3] != "  0") {
+                            let mut atomMapNumber = 0;
+                            atomMapNumber =
+                                line[60..60 + 3].trim().parse().unwrap();
+                            res.set_molAtomMapNumber(atomMapNumber);
+                        }
+                        if (line.len() >= 66 && &line[63..63 + 3] != "  0") {
+                            let mut inversionFlag = 0;
+                            inversionFlag =
+                                line[63..63 + 3].trim().parse().unwrap();
+                            res.set_molInversionFlag(inversionFlag);
+                        }
+                        if (line.len() >= 69 && &line[66..66 + 3] != "  0") {
+                            let mut exactChangeFlag = 0;
+                            exactChangeFlag =
+                                line[66..66 + 3].trim().parse().unwrap();
+                            res.set_molExactChangeFlag(exactChangeFlag);
+                        }
+                        return res;
+
                         todo!();
                     };
                     let aid = mol.add_atom(atom);
